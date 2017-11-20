ae57a22c2498139865e06a97850a5014d7a760f3
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-11-20 16:43:12 +0900

    add shared scheme

---
+++ Carthage/RxSwifter/RxSwifter/RxSwifter.h
@@ -0,0 +1,19 @@
+//
+//  RxSwifter.h
+//  RxSwifter
+//
+//  Created by Tomoya Hirano on 2017/11/20.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSwifter.
+FOUNDATION_EXPORT double RxSwifterVersionNumber;
+
+//! Project version string for RxSwifter.
+FOUNDATION_EXPORT const unsigned char RxSwifterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwifter/PublicHeader.h>
+
+
